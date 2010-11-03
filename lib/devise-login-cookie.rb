399e6701166ed90df356245b4259d823b58470af
Paul Annesley <paul@99designs.com>
2010-11-04 00:49:35 +1100

    Signed cookie set on auth, contains [ resource.id, Time.now.to_i ]

--- lib/devise-login-cookie.rb
+++ lib/devise-login-cookie.rb
@@ -1,2 +1,26 @@
 module DeviseLoginCookie
+
+  def success!(resource)
+    super
+    if succeeded?
+      cookies.signed["login_#{scope}_token"] = cookie_values(resource)
+    end
+  end
+
+  #########
+  protected
+
+  def cookie_values(resource)
+    value = [ resource.id, Time.now.to_i ]
+    options = Rails.configuration.session_options.slice(:path, :domain, :secure)
+    options.merge! :value => value
+    options
+  end
+
+  def succeeded?
+    @result == :success
+  end
+
 end
+
+Devise::Strategies::Authenticatable.send :include, DeviseLoginCookie
