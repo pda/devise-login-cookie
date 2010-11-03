bf3fd51a9cd6055bda691c3d9cf8336a28872193
Paul Annesley <paul@99designs.com>
2010-11-03 17:56:21 +1100

    Generated new 'devise-login-cookie' gem using Bundler.

---
+++ lib/devise-login-cookie.rb
@@ -0,0 +1,7 @@
+module Devise
+  module Login
+    module Cookie
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/devise-login-cookie/version.rb
@@ -0,0 +1,7 @@
+module Devise
+  module Login
+    module Cookie
+      VERSION = "0.0.1"
+    end
+  end
+end
